4cf5560a472bce8649fe22525719bca63ac411f8
Miguel Quezada <mig.aquezada@gmail.com>
2022-07-25 21:38:41 -0400

    add pods and todocell

---
+++ Todo/TodoCell.h
@@ -0,0 +1,20 @@
+//
+//  TodoCell.h
+//  
+//
+//  Created by Miguel Quezada on 25-07-22.
+//
+
+#import "MaterialButtons.h"
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TodoCell : UICollectionViewCell
+
+@property(nonatomic, copy) NSString *text;
+@property(nonatomic, readonly) MDCButton *deleteButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Todo/TodoSectionController.h
@@ -0,0 +1,16 @@
+//
+//  TodoSectionController.h
+//  Todo
+//
+//  Created by Miguel Quezada on 25-07-22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TodoSectionController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
